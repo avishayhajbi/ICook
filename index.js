4023d8223186c43e18931f739cc77ef7934ed3ba
avishay <avishayhajbi@gmail.com>
2015-04-04 22:47:33 +0300

    update server to npm install

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ Db = require('mongodb').Db,
     GridStore = require('mongodb').GridStore,
     Grid = require('mongodb').Grid,
     Code = require('mongodb').Code,
-    BSON = require('mongodb').pure().BSON,
+    //BSON = require('mongodb').pure().BSON,
     assert = require('assert');
 
 app.use(express.static(path.join(__dirname ,'views')));
@@ -31,48 +31,14 @@ var port = process.env.PORT || 8080;
 app.set('port', port);
 app.set('view engine', 'ejs');
 
-/*if (port == 8080)
-  config ={
-  	host:"127.0.0.1",
-  	user:"root",
-  	password:"",
-  	database:"lecturus",
-  	port: 3306,
-    mongoUrl:'mongodb://localhost:27017/lecturus'
-  }
-else*/
-/*
-  config ={
-  	host: "us-cdbr-iron-east-01.cleardb.net",
-    user: "b23c6d0f964532",
-    password: "1fc1c4ed",
-    database: "heroku_f00102faee97288",
-    port: 3306,
-    mongoUrl:'mongodb://lecturus:lec123@ds033477.mongolab.com:33477/heroku_app33687705'
-  }
+config ={
+  mongoUrl:'mongodb://lecturus:lec123@ds033477.mongolab.com:33477/heroku_app33687705'
+}
 
-
-pool = mysql.createPool(config);
-*/
 app.listen(app.get('port'), function () {
     console.log('ICook Server running...'+app.get('port'));
 });
-/*
-function keepAlive(){
-  try{
-    pool.getConnection(function(err, connection){
-    if(err) { return; }
-    connection.ping();
-    connection.end();
-    });
-  }catch(err){}
-}
-setInterval(keepAlive, 30000);
-*/
-/*
-var lec_users = require('./s_users'); // can use app.use( '/folderName' ,require('lecturus_users'));
-app.use(lec_users); 
-*/
+
 app.get('/', function (req, res) {
   res.render('index',{
 		title:"ICook"
@@ -83,27 +49,3 @@ app.get('/*', function (req, res) {
 	res.send(405,'page not allowed lecturus')
 });
 
- /*MongoClient.connect(config.mongoUrl, {native_parser:true}, function(err, db) {
-      if (err) return;
-      assert.equal(null, err);
-      // insert the jsons below
-      //db.collection('coll_test').insert([{a:1}, {b:1}, {c:2}], {upsert:true}, function(err, result) {
-      // update the first match a:1 to b:8 if not exist insert
-      db.collection('coll_test').update({a:1}, {b:8}, {upsert:true}, function(err, result) { 
-        assert.equal(null, err);
-        //assert.equal(1, result);
-        console.log('done update mongodb')
-        db.close();
-        mongocallback();
-      });
-    });*/
-
-/*var mongocallback = function(){
-  MongoClient.connect(config.mongoUrl, {native_parser:true}, function(err, db) {
-    var collection = db.collection('coll_test');
-    collection.find().toArray(function (err, docs) {
-      //console.log(docs)
-      db.close();
-    });
-  });
-}*/
