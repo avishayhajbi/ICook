b1a7f5ea7e2f5c9b2163a818712ea62c515d412f
avishay <avishayhajbi@gmail.com>
2015-04-07 23:25:42 +0300

    update categories model

--- models/categories.js
+++ models/categories.js
@@ -4,56 +4,78 @@ var Schema = mongoose.Schema;
 var sessionsSchema = new Schema( 
 {
     lang: String,
-    category: [
-        {
-            id: Number,
-            key: String,
-            url: String
-        }
-    ],
-    user: [
-        {
-            id: Number,
-            key: String,
-            url: String
-        }
-    ],
-    dairy: [
-        {
-            id: Number,
-            key: String,
-            url: String
-        }
-    ],
-    kosher: [
-        {
-            id: Number,
-            key: String,
-            url: String
-        }
-    ],
-    forWho: [
-        {
-            id: Number,
-            key: String,
-            url: String
-        }
-    ],
-    accessories: [
-        {
-            id: Number,
-            key: String,
-            url: String
-        }
-        
-    ],
-    specialPopulations: [
-        {
-            id: Number,
-            key: String,
-            url: String
-        }
-    ]
+    length:Number,
+    issues:{ type : Array , "default" : [] },
+    category: {
+        question:String,
+        res:[
+            {
+                id: Number,
+                key: String,
+                url: String
+            }
+        ]
+    },
+    user: {
+        question:String,
+        res:[
+            {
+                id: Number,
+                key: String,
+                url: String
+            }
+        ]
+    },
+    dairy: {
+        question:String,
+        res:[
+            {
+                id: Number,
+                key: String,
+                url: String
+            }
+        ]
+    },
+    kosher: {
+        question:String,
+        res:[
+            {
+                id: Number,
+                key: String,
+                url: String
+            }
+        ]
+    },
+    forWho: {
+        question:String,
+        res:[
+            {
+                id: Number,
+                key: String,
+                url: String
+            }
+        ]
+    },
+    accessories: {
+        question:String,
+        res:[
+            {
+                id: Number,
+                key: String,
+                url: String
+            }
+        ]
+    },
+    specialPopulations: {
+        question:String,
+        res:[
+            {
+                id: Number,
+                key: String,
+                url: String
+            }
+        ]
+    },
 	
 });
 	
