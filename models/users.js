e9fe8bc4d860fa7a8fff724b81dc044ca6b3173e
avishay <avishayhajbi@gmail.com>
2015-04-21 16:54:35 +0300

    insert email to user favorites and recipes

--- models/users.js
+++ models/users.js
@@ -6,8 +6,8 @@ var usersSchema = new Schema(
 		email: String,
 	    name: String,
 	    image:String,
-	    favorites:  [{ id : String, forWho:{ type : Array , "default" : [] }, name:String , default : [] }],
-	    recipes:  [{ id : String, forWho:{ type : Array , "default" : [] }, name:String , default : [] }],
+	    favorites:  [{ id : String, email: String, forWho:{ type : Array , "default" : [] }, name:String , default : [] }],
+	    recipes:  [{ id : String, email: String, forWho:{ type : Array , "default" : [] }, name:String , default : [] }],
 	});
 	
 User =mongoose.model('users', usersSchema);
